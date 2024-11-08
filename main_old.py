3f4a6ae4f4ab33d1b09551242bc531716159b3a5
statelinecat <statelinecat@gmail.com>
2024-11-09 02:10:32 +0300

    add code 2

--- main_old.py
+++ main_old.py
@@ -1,11 +1,12 @@
 import asyncio
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher, F, types, Router
 from aiogram.filters import CommandStart, Command
-from aiogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.types import Message, CallbackQuery, InlineKeyboardMarkup, InlineKeyboardButton
 
 import random
 import requests
 from config import TOKEN
+
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
@@ -25,21 +26,30 @@ zodiac_signs = [
     ("Рыбы", "pisces")
 ]
 
+
 @dp.message(Command('help'))
 async def help(message: Message):
-    await message.answer("По команде /start сообщаю гороскоп на сегодня.")
+   await message.answer("По команде /start сообщаю гороскоп на сегодня.")
 
 @dp.message(CommandStart())
 async def send_welcome(message: Message):
     keyboard = InlineKeyboardMarkup()
     for sign, data in zodiac_signs:
-        keyboard.add(InlineKeyboardButton(text=sign, callback_data=data))
+            keyboard.add(InlineKeyboardButton(text=sign, callback_data=data))
     await message.answer("Выберите знак зодиака:", reply_markup=keyboard)
 
+
+
+# async def main():
+#    await dp.start_polling(bot)
+
 async def main():
+    # Подключение обработчиков
+    # dp.include_router()
     # Запуск поллинга
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
+
 if __name__ == '__main__':
-    asyncio.run(main())
+   asyncio.run(main())
